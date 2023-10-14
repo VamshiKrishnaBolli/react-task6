cc5b2fccaabb15edb742b37aa76a83a267b886a7
VamshiKrishnaBolli <vamshikrishnabolli@gmail.com>
2023-10-14 13:15:25 +0530

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>
+        I'm always open to new opportunities and collaborations. 
+      </p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a href="vamshikrishnabolli@gmail.com">Email</a>
+        </li>
+        <li>
+          <strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/vamshi-krishna-94934b220?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer">Linkedin</a>
+        </li>
+        <li>
+          <strong>GitHub:</strong> <a href=" https://github.com/VamshiKrishnaBolli" target="_blank" rel="noopener noreferrer">github</a>
+        </li>
+       
+      </ul>
+      <p>
+        I'm looking forward to connecting with you!
+      </p>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>BOLLI VAMSHI KRISHNA</h1>
+      <p>Web Developer</p>
+    </header>
+  );
+}
+
+export default Header;
